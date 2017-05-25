d9fc827c967770aeedcd0604fa83faffc6c66d37
Jonathan Gill <jtgill@email.arizona.edu>
2017-05-25 14:37:19 -0700

    General Modifications
    Split data loading into a separate file
    Added more comments to the single-layer classifier code

--- spam-ham-one-layer.py
+++ spam-ham-one-layer.py
@@ -1,51 +1,51 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-from keras.losses import mean_squared_error
 from keras import optimizers
 
-import tarfile
-import os
 import numpy as np
+from dataloader import *
 
-def csv_to_numpy_array(filePath, delimiter):
-    return np.genfromtxt(filePath, delimiter=delimiter, dtype=None)
-
-def import_data():
-    if "data" not in os.listdir(os.getcwd()):
-        # Untar directory of data if we haven't already
-        tarObject = tarfile.open("data.tar.gz")
-        tarObject.extractall()
-        tarObject.close()
-        print("Extracted tar to current directory")
-    else:
-        # we've already extracted the files
-        pass
-
-    print("loading training data")
-    trainX = csv_to_numpy_array("data/trainX.csv", delimiter="\t")
-    trainY = csv_to_numpy_array("data/trainY.csv", delimiter="\t")
-    print("loading test data")
-    testX = csv_to_numpy_array("data/testX.csv", delimiter="\t")
-    testY = csv_to_numpy_array("data/testY.csv", delimiter="\t")
-    return trainX,trainY,testX,testY
+np.set_printoptions(threshold=np.nan)
 
 trainX,trainY,testX,testY = import_data()
-print(testY[0])
 
+# Just want to see what the data looks like
+print(trainX[0])
+print(trainY[0])
+print("Shape of training data:", trainX.shape)
+print("Shape of training labels:", trainY.shape)
+
+# The code below literally does everything that the spam-ham
+# TF program did.
 num_features = trainX.shape[1]
 num_labels = trainY.shape[1]
 
+# Define a model with sequential layers (not related to RNN's or time series).
+# Sequential just means that we're stacking layers of activations on top of
+# each other.
 model = Sequential()
-#model.add(Dense(int(num_features/10), input_dim=num_features, activation='relu'))
+# Add a layer to the model (this is a dense, e.g. fully-connected, layer).
 model.add(Dense(num_labels, input_dim=num_features, activation='softmax'))
+
+# Number of epochs we want to run through.
 num_epochs = 3000
 
+# Create an optimizer with special parameters (could have alternatively passed)
+# the 'optimizer' parameter the string "sgd" for default learning parameters.
 sgd = optimizers.SGD(lr=0.0008, decay=1e-6, momentum=0.9, nesterov=True)
+
+# Categorical cross-entropy means that you're using one-hot encodings for the
+# true class prediction. A metric is what's used to evaluate the model in the
+# evaluation step.
 model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
+# The fitting procedure is pretty neat; it returns a 'History' type object
+# that has info collected during training (error rates, accuracy, etc.)
 history = model.fit(trainX, trainY, batch_size=128, nb_epoch=num_epochs,
 	verbose=1, validation_split=0.3)
 
+# Runs the trained model on the given set of test data, measuring the 
+# metric we supplied in the .compile() method (accuracy, in our case).
 score = model.evaluate(testX, testY, verbose=0)
 
 print('Test score:', score[0])
--- spam-ham-two-layers.py
+++ spam-ham-two-layers.py
@@ -1,33 +1,11 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-from keras.losses import mean_squared_error
 from keras import optimizers
 
-import tarfile
-import os
 import numpy as np
+from dataloader import *
 
-def csv_to_numpy_array(filePath, delimiter):
-    return np.genfromtxt(filePath, delimiter=delimiter, dtype=None)
-
-def import_data():
-    if "data" not in os.listdir(os.getcwd()):
-        # Untar directory of data if we haven't already
-        tarObject = tarfile.open("data.tar.gz")
-        tarObject.extractall()
-        tarObject.close()
-        print("Extracted tar to current directory")
-    else:
-        # we've already extracted the files
-        pass
-
-    print("loading training data")
-    trainX = csv_to_numpy_array("data/trainX.csv", delimiter="\t")
-    trainY = csv_to_numpy_array("data/trainY.csv", delimiter="\t")
-    print("loading test data")
-    testX = csv_to_numpy_array("data/testX.csv", delimiter="\t")
-    testY = csv_to_numpy_array("data/testY.csv", delimiter="\t")
-    return trainX,trainY,testX,testY
+np.set_printoptions(threshold=np.nan)
 
 trainX,trainY,testX,testY = import_data()
 print(testY[0])
